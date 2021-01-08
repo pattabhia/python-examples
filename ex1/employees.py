472d9cde8bd8bb00e84fbd4e0cf9ce0e8ff28528
pattabhia <shivaram2004@gmail.com>
2021-01-08 09:55:18 +0530

    list & tuples

---
+++ ex1/employees.py
@@ -0,0 +1,18 @@
+list = ["ram", "rahim", 'akbar', 'amar']
+print(list);
+print(list[0]);
+print(list[1]);
+print(list[2]);
+print(list[3]);
+#Exception Handling
+#Loop List
+list[3] = 'Sunil';
+print(list*3)
+print(list[2][1])
+if("akbar" in list) :
+    print('yes')
+else :
+    print('no')
+tuple = ("ram", "rahim", 'akbar', 'amar')
+print(tuple[0])
+#Dictionaries - > Object value
