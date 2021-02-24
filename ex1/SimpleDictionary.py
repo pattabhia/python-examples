8bae011a1fce033303bee4af878eb55fc1c65e6c
pattabhia <shivaram2004@gmail.com>
2021-02-24 11:07:23 +0530

    Simple Dictionary

---
+++ ex1/SimpleDictionary.py
@@ -0,0 +1,4 @@
+stateCapital = {'karnataka': 'bengaluru', 'telangana': 'hyderabad', 'maharashtra': 'mumbai',
+                'andhra pradesh': 'vijayawada'}
+print(stateCapital.keys())
+print(stateCapital.values())
