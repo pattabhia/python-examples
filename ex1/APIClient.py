492a81f2ac26f8b797e87717b77da0c94a1acd74
pattabhia <shivaram2004@gmail.com>
2021-04-06 16:39:55 +0530

    Fixing issues

--- ex1/APIClient.py
+++ ex1/APIClient.py
@@ -1,6 +1,6 @@
 import requests
 try:
-    response = requests.get("http://localhost:50002/user/10")
+    response = requests.get("http://localhost:50002/user/20")
     print(response.json())
 except requests.exceptions.HTTPError as error:
     print(error)
