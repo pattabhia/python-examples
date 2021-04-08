bc7d841548208db7ee1303c023fce81449f7335d
pattabhia <shivaram2004@gmail.com>
2021-04-08 18:11:02 +0530

    removing sample files

--- user-service/message/welcome.py
+++
@@ -1,3 +0,0 @@
-def show(key):
-    print(key)
-    return
--- user-service/testmessage.py
+++ user-service/testmessage.py
@@ -1,6 +1,2 @@
-from message import welcome
-import hello
-from employees import emp
-welcome.show("1")
-hello.printme("greetings")
-emp.display_name("welcome")
+from message.welcome import show
+show("1")
