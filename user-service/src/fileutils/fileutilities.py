ec047a4bd1a37fefe6674148c5197c495e843d8a
pattabhia <shivaram2004@gmail.com>
2021-04-09 17:06:15 +0530

    read from prop file

--- user-service/src/fileutils/fileutilities.py
+++ user-service/src/fileutils/fileutilities.py
@@ -1,6 +1,23 @@
+from jproperties import Properties
+
+configs = Properties()
+
+
 def exists(file):
     try:
         with open(file) as f:
             print('file exists')
     except IOError:
         print('file not accessible')
+
+
+def getproperty():
+    try:
+        with open('app-config.properties', 'rb') as config_file:
+            configs.load(config_file)
+    except IOError:
+        print('prop file error')
+
+
+getproperty()
+print(configs.get("csv.read.filepath"))
