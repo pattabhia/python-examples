599fdad7b42e02182ebf859e40e35d97100312d1
pattabhia <shivaram2004@gmail.com>
2021-04-12 13:13:31 +0530

    property file initialization code is moved to init script

--- user-service/src/fileutils/__init__.py
+++ user-service/src/fileutils/__init__.py
@@ -1,1 +1,36 @@
-from .properties import *
+from pathlib import Path
+
+from jproperties import Properties
+
+
+class BaseProperties:
+    __configs = object()
+
+    def __init__(self):
+        print('BaseProperties = Constructor')
+        self.__configs = Properties()
+        self.init_properties()
+
+    def exists(self, file: str) -> None:
+        try:
+            with open(file) as f:
+                print('file exists', f.name)
+        except IOError:
+            print('file not accessible')
+        return
+
+    def init_properties(self):
+        project_root = Path(__file__).parents[2]
+        prop_file_path = project_root / 'resources/app-config.properties'
+        try:
+            with open(prop_file_path, 'rb') as config_file:
+                self.get_config().load(config_file)
+        except Exception as ex:
+            print('prop file error', str(ex))
+        return
+
+    def get_property(self, key) -> str:
+        return self.get_config().get(key)
+
+    def get_config(self):
+        return self.__configs
--- user-service/src/fileutils/properties.py
+++ user-service/src/fileutils/properties.py
@@ -1,37 +1,11 @@
-from pathlib import Path
-
-from jproperties import Properties
+from fileutils import BaseProperties
 
 
-class AppProperties:
-    __configs = object()
+class AppProperties(BaseProperties):
 
     def __init__(self):
         print('AppProperties = Constructor')
-        self.__configs = Properties()
-        self.init_properties()
 
-    def exists(self, file: str) -> None:
-        try:
-            with open(file) as f:
-                print('file exists', f.name)
-        except IOError:
-            print('file not accessible')
-        return
-
-    # TODO: add this feature to init script
-    def init_properties(self):
-        project_root = Path(__file__).parents[2]
-        prop_file_path = project_root / 'resources/app-config.properties'
-        try:
-            with open(prop_file_path, 'rb') as config_file:
-                self.get_config().load(config_file)
-        except Exception as ex:
-            print('prop file error', str(ex))
-        return
-
-    def get_property(self, key) -> str:
-        return self.get_config().get(key)
-
-    def get_config(self):
-        return self.__configs
+    @staticmethod
+    def get_property(key):
+        return BaseProperties().get_property(key)
