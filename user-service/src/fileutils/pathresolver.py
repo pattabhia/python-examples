cdb4014955a277f0bdca1106240c274d614c75ac
pattabhia <shivaram2004@gmail.com>
2021-04-14 11:30:05 +0530

    Applying SRP

--- user-service/src/fileutils/pathresolver.py
+++ user-service/src/fileutils/pathresolver.py
@@ -1,9 +1,9 @@
 import os
 
 
-def resolve_path(basepath, file_name):
+def resolve_path(path, file_name):
     if "data" in str(file_name):
-        return os.path.join(basepath.data, file_name.data)
+        return os.path.join(path.data, file_name.data)
     else:
         print('incorrect path given for csv file')
     return
