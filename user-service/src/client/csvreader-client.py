92e1081fdd6e07786c4360d614d8f44ee663cfea
pattabhia <shivaram2004@gmail.com>
2021-04-15 21:08:18 +0530

    update

--- user-service/src/client/csvreader-client.py
+++ user-service/src/client/csvreader-client.py
@@ -1,6 +1,7 @@
-from fileutils.properties import AppProperties
-from fileutils.pathresolver import resolve_path
 from csvreader.csvread import CSVReader
+from fileutils.pathresolver import resolve_path
+from fileutils.properties import AppProperties
+
 
 propObj = AppProperties()
 file = resolve_path(propObj.get_property('base.path'), propObj.get_property('csv.read.filepath'))
